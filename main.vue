7453946f3991e3125c96b4d1f89cfe1683bb3f76
DreaMinder <dreaminder7@gmail.com>
2018-06-21 16:01:06 +0300

    initial

---
+++ main.vue
@@ -0,0 +1,21 @@
+<template>
+  <span>
+    <template v-for="(chunk, i) in chunks">
+      <span v-html="chunk" v-if="i % 2 === 0" />
+      <slot v-else :name="chunk" />
+    </template>
+  </span>
+</template>
+
+<script>
+
+export default {
+  props: ['content'],
+  computed: {
+    chunks(){
+      return this.content.split('%%')
+    }
+  }
+}
+
+</script>
